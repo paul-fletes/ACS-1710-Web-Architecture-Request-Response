b17afb17366ba8c5cae2d2f42805af2994548e43
Paul Fletes <paul.fletes@gmail.com>
2022-10-27 14:17:37 -0500

    import flask and set up homepage route

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
-
+from flask import Flask
 # TODO: Follow the assignment instructions to complete the required routes!
 # (And make sure to delete this TODO message when you're done!)
+app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+    return 'Are you there, world? It\'s me, Paul!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
