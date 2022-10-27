7076244d3b5c264452f82b9ed8a3f459ce1e0ee3
Paul Fletes <paul.fletes@gmail.com>
2022-10-27 15:25:28 -0500

    add /animal/<users_animal> route

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def homepage():
     return 'Are you there, world? It\'s me, Paul!'
 
 
+@app.route('/animal/<users_animal>')
+def fave_animal(users_animal):
+    return f'Wow, {users_animal} is my favorite animal, too!'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
